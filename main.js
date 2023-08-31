1cb3a471ee0ebde94efea87efa2b17ba266f4328
FabioCostantino84 <costantinofabio84@gmail.com>
2023-08-31 14:53:27 +0200

    add print to page

--- main.js
+++ main.js
@@ -2,21 +2,31 @@
 
 Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente con un ciclo while.
 
+## tools:
+- const
+- array
+- console log
+- while cicle
+- getElement
 
 */
 
 
-
+//shopping list
 const shoppingList = ['pomodori', 'peperoni', 'insalata', 'uova', 'pasta', 'olio', 'sale'];
 
 console.log(shoppingList);
 
+
 let i = 0;
 
+//cycle to print in console
 while ( i < shoppingList.length) {
     const shopping = shoppingList[i];
     console.log(shopping);
-
-
+    
     i++
 }
+
+//print to page
+document.getElementById("list").innerHTML = shoppingList;
